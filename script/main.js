c6ddc0177045c72528ce1dadb3e0654877938caa
matsgoran <matsgora@gmail.com>
2022-03-05 11:56:40 +0100

    Ikke fjern mellomrom i tekst

--- script/main.js
+++ script/main.js
@@ -35,7 +35,7 @@ const animationTimeline = () => {
     .join("</span><span>")}</span`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
+    .split(" ")
     .join("</span><span>")}</span`;
 
   const ideaTextTrans = {
